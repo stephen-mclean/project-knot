4ab5d44ebc07d5361bda456ca670769a7f50e355
Stephen McLean <smclean@guidewire.com>
2019-02-26 13:44:43 +0000

    Add font awesome dependency. Add accordion component.

---
+++ src/fontawesome/initFaIcons.js
@@ -0,0 +1,7 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronRight,
+  faChevronDown
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChevronRight, faChevronDown);
