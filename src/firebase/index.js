fe4af1d83d4c97a453086b7718a43aef000d4d8f
Stephen McLean <smclean@guidewire.com>
2019-02-27 09:48:57 +0000

    Initialize firebase on app start.

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID
+});
