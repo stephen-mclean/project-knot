a12ac69c322f877a015ea154ce3bc2353d78b606
Stephen McLean <smclean@SMCLEAN-MBP15.guidewire.com>
2019-08-14 16:38:31 +0100

    Updated wording for home, wedding and after party pages

--- src/pages/AfterParty/AfterParty.js
+++ src/pages/AfterParty/AfterParty.js
@@ -20,6 +20,11 @@ const Description = styled(B2)`
   margin-bottom: 1rem;
 `;
 
+const P = styled(B2)`
+  margin-bottom: 1rem;
+  color: ${props => props.theme.colors.foreground.secondary};
+`;
+
 export default () => (
   <PageWithNav>
     <Fragment>
@@ -27,11 +32,36 @@ export default () => (
         <MainHeader>12th October 2019</MainHeader>
         <VenueInfo>Arnolds Hotel</VenueInfo>
       </HeadingContainer>
-      <Description>Description here</Description>
+      <Description>
+        If your feet and head are not too delicate, we would be delighted if you
+        could join us for some more fun on Saturday, 12th October in Arnolds
+        Hotel, Dunfanaghy. We will be meeting from 6.30pm with food served at
+        7pm and music to follow.
+      </Description>
 
-      <Accordion title="Accommodation" content="Info here" />
-      <Accordion title="Directions" content="Info here" />
-      <Accordion title="Useful Numbers" content="Info here" />
+      <Accordion title="Accommodation">
+        <P>
+          For those that would like to stay overnight, Arnolds Hotel have
+          offered a special rate of €85 B&B per double/twin room based on 2
+          adults sharing and €75 B&B per single occupancy room. Please quote “Mc
+          Hugh/Kerr wedding” when booking to avail of the discount (Tel
+          074-9136208)
+        </P>
+      </Accordion>
+      <Accordion title="Directions">
+        <iframe
+          title="Directions"
+          width="300"
+          height="300"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=arnolds%20hotel&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+        />
+      </Accordion>
+      <Accordion title="Transport">
+        <P>Dunfanaghy Taxi 085 7870164</P>
+      </Accordion>
     </Fragment>
   </PageWithNav>
 );
--- src/pages/WeddingDay/WeddingDay.js
+++ src/pages/WeddingDay/WeddingDay.js
@@ -20,6 +20,15 @@ const Description = styled(B2)`
   margin-bottom: 1rem;
 `;
 
+const P = styled(B2)`
+  margin-bottom: 1rem;
+  color: ${props => props.theme.colors.foreground.secondary};
+`;
+
+const HotelName = styled(B2)`
+  text-decoration: underline;
+`;
+
 export default () => (
   <PageWithNav>
     <Fragment>
@@ -27,11 +36,60 @@ export default () => (
         <MainHeader>11th October 2019</MainHeader>
         <VenueInfo>Rock Hill House</VenueInfo>
       </HeadingContainer>
-      <Description>Description here</Description>
+      <Description>
+        Our wedding ceremony will take place at 2pm in The Gallery at Rockhill
+        House and the evening reception will follow in Rockhill House.
+      </Description>
 
-      <Accordion title="Accommodation" content="Accommodation info here" />
-      <Accordion title="Directions" content="Info here" />
-      <Accordion title="Useful Numbers" content="Info here" />
+      <Accordion title="Accommodation">
+        <P>
+          There are a limited number of bedrooms available at Rockhill House and
+          we would be delighted to have as many of our friends and family stay
+          with us. If you are interested in reserving a room please contact
+          Catriona (086-3777948) or Darren (085-1319941).
+        </P>
+
+        <P>
+          Alternatively, we have secured special rates in the following hotels,
+          both of which are 5-8mins drive from Rockhill House. If making a
+          reservation at either hotel please quote “Mc Hugh/Kerr wedding” to
+          avail of their special rate.
+        </P>
+
+        <P>
+          <HotelName>Radisson Hotel, Letterkenny (074-9194444)</HotelName>
+          €84 B&B per double/twin room based on 2 adults sharing or €70 B&B per
+          single occupancy room.
+        </P>
+
+        <P>
+          <HotelName>Station House Hotel, Letterkenny (074-9123100)</HotelName>
+          €60 B&B per person sharing or €80 B&B for single occupancy room.
+        </P>
+      </Accordion>
+
+      <Accordion title="Directions">
+        <iframe
+          title="Directions"
+          width="300"
+          height="300"
+          src="https://maps.google.com/maps?q=rock%20hill%20house&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+        />
+      </Accordion>
+
+      <Accordion title="Transport">
+        <P>
+          A bus will be available to take guests from Rockhill House to The
+          Radisson or Station House Hotel between 1-2am. For any guests that
+          would like to depart earlier in the evening local taxis will be
+          available.
+        </P>
+
+        <P>Letterkenny Cabs 074 9127000</P>
+        <P>High Road Taxi Cabs 086 2462355</P>
+      </Accordion>
     </Fragment>
   </PageWithNav>
 );
